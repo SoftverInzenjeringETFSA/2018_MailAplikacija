7ffe7614e1bbc0f4e25bfb110abb74c122def801
elvircn <elvircrn@gmail.com>
2018-05-12 23:23:59 +0200

    Get word dictionary from bg dataset

--- MailApp/backend/SpamFilter/data.py
+++ MailApp/backend/SpamFilter/data.py
@@ -15,16 +15,17 @@ def get_txts_recursive(path):
 def strip_html_tags(text):
     return re.sub('<[^<]+?>', '', text)
 
+
 def union_sets(x, y):
     return x.union(y)
 
 
 def extract_bg_words(path):
     def get_email_content(lines):
-        return lines[(lines.index("\n") + 1):]
+        return lines[(lines.index("\n") + 1):] if "\n" in lines else []
 
-    def remove_non_alphanum(lowered_lines):
-        return [re.sub('![a-z0-9]', ' ', line) for line in lowered_lines]
+    def remove_non_alphanum(lowered):
+        return [re.sub('[^a-z0-9]+', ' ', line) for line in lowered]
 
     def get_word_set(line):
         return set(line.split())
@@ -36,6 +37,10 @@ def extract_bg_words(path):
         raw_lines = f.readlines()
         # Content starts from newline
         content = get_email_content(raw_lines)
+
+        if len(content) == 0:
+            return set()
+
         # Strip tags
         stripped_tags = map(strip_html_tags, content)
         # To lower
@@ -46,10 +51,14 @@ def extract_bg_words(path):
 
 
 def preprocess_bg():
-    files = list(get_txts_recursive(dm.BG_DATASET_PATH))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        word_set = reduce(union_sets, executor.map(extract_bg_words, files))
-        print(word_set)
+    def get_word_dict():
+        files = list(get_txts_recursive(dm.BG_DATASET_PATH))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            word_set = reduce(union_sets, executor.map(extract_bg_words, files))
+            word_dict = {}
+            for idx, word in enumerate(word_set):
+                word_dict[word] = idx
+    return get_word_dict()
 
 
 def seq_preprocess_bg():
@@ -62,4 +71,5 @@ def preprocess_enron():
 
 
 def preprocess():
-    preprocess_bg()
+    word_dict = preprocess_bg()
+    print(word_dict)
